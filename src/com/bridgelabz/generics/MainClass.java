a9857e3f91289c32a6abc34ef056711bdf531ecb
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-25 17:47:55 +0530

    Modified to use generic class

--- src/com/bridgelabz/generics/MainClass.java
+++ src/com/bridgelabz/generics/MainClass.java
@@ -2,24 +2,11 @@ package com.bridgelabz.generics;
 
 public class MainClass {
     public static void main(String[] args) {
-        MaxValue maxValue = new MaxValue();
-        System.out.println("Integer Test case 1");
-        maxValue.maxvalue(5,4,3);
-        System.out.println("Integer Test case 2");
-        maxValue.maxvalue(5,6,3);
-        System.out.println("Integer Test case 3");
-        maxValue.maxvalue(5,4,7);
-        System.out.println("Float Test case 1");
-        maxValue.maxvalue(5.2F,4.3F,3.2F);
-        System.out.println("Float Test case 2");
-        maxValue.maxvalue(5.2F,6.3F,3.2F);
-        System.out.println("Float Test case 3");
-        maxValue.maxvalue(5.2F,4.3F,7.2F);
-        System.out.println("String Test case 1");
-        maxValue.maxvalue("Peach","Apple","Banana");
-        System.out.println("String Test case 2");
-        maxValue.maxvalue("Apple","Zach","Banana");
-        System.out.println("String Test case 3");
-        maxValue.maxvalue("Apple","Banana","Class");
+        MaxValue<Integer> maxValue = new MaxValue<>(5,4,3);
+        maxValue.testMaximum();
+        MaxValue<Float> maxValue1 = new MaxValue<>(5.2F,8.3F,3.2F);
+        maxValue1.testMaximum();
+        MaxValue<String> maxValue2 = new MaxValue<>("Peach","Apple","Zach");
+        maxValue2.testMaximum();
     }
 }
