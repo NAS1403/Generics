4fd14e9e4a65a3121d06b1c6f770f0bfe7e69e6f
Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-07-25 15:03:29 +0530

    Refactored to generic method

--- src/com/bridgelabz/generics/MainClass.java
+++ src/com/bridgelabz/generics/MainClass.java
@@ -2,12 +2,24 @@ package com.bridgelabz.generics;
 
 public class MainClass {
     public static void main(String[] args) {
-        MaxString maxString = new MaxString();
-        System.out.println("Test case 1");
-        maxString.maxvalue("Peach","Apple","Banana");
-        System.out.println("Test case 2");
-        maxString.maxvalue("Apple","Zach","Banana");
-        System.out.println("Test case 3");
-        maxString.maxvalue("Apple","Banana","Class");
+        MaxValue maxValue = new MaxValue();
+        System.out.println("Integer Test case 1");
+        maxValue.maxvalue(5,4,3);
+        System.out.println("Integer Test case 2");
+        maxValue.maxvalue(5,6,3);
+        System.out.println("Integer Test case 3");
+        maxValue.maxvalue(5,4,7);
+        System.out.println("Float Test case 1");
+        maxValue.maxvalue(5.2F,4.3F,3.2F);
+        System.out.println("Float Test case 2");
+        maxValue.maxvalue(5.2F,6.3F,3.2F);
+        System.out.println("Float Test case 3");
+        maxValue.maxvalue(5.2F,4.3F,7.2F);
+        System.out.println("String Test case 1");
+        maxValue.maxvalue("Peach","Apple","Banana");
+        System.out.println("String Test case 2");
+        maxValue.maxvalue("Apple","Zach","Banana");
+        System.out.println("String Test case 3");
+        maxValue.maxvalue("Apple","Banana","Class");
     }
 }
